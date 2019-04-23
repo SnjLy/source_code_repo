b8af236cffee2a3e439fbfdb10512e42825c433a
yehao <1223952541@qq.com>
2019-04-23 16:39:23 +0800

    两个链表数据相加

--- src/main/java/algorithm/leetecode/twosum/TwoLinkedSum.java
+++ src/main/java/algorithm/leetecode/twosum/TwoLinkedSum.java
@@ -9,8 +9,63 @@ package algorithm.leetecode.twosum;
  */
 public class TwoLinkedSum {
 
+    public static void main(String[] args) {
+        TwoLinkedSum tls = new TwoLinkedSum();
+        tls.test();
+    }
+
+    public void test(){
+        ListNode p1 = new ListNode(2);
+        ListNode p2 = new ListNode(4);
+        ListNode p3 = new ListNode(3);
+        p1.next = p2;
+        p2.next = p3;
+
+        ListNode q1 = new ListNode(5);
+        ListNode q2 = new ListNode(6);
+        ListNode q3 = new ListNode(4);
+        q1.next = q2;
+        q2.next = q3;
+        ListNode first = addTwoNumbers(p1, q1);
+        print(first);
+    }
+
+    public void print(ListNode p){
+        while (p != null) {
+            System.out.println(p.val);
+            if (p != null) {
+                p = p.next;
+            }
+        }
+    }
+
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        return null;
+        //反向遍历， 加法进位
+        //carry标识进位
+        int carry = 0;
+        //新链表头结点
+        ListNode first = new ListNode(0);
+        ListNode p = l1, q= l2, curr = first;
+
+        //链表长度不同
+        while (p != null || q != null) {
+            int x = p != null ? p.val : 0;
+            int y = q != null ? q.val : 0;
+            int sum = x + y + carry;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (p != null){
+                p = p.next;
+            }
+            if (q!=null){
+                q = q.next;
+            }
+        }
+        if (carry > 0){
+            curr.next = new ListNode(carry);
+        }
+        return first.next;
     }
 
 
